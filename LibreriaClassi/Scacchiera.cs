22957d1e3f47a6eae3071a54ecc07eb7feba4a58
narcisbuzatu2003@gmail.com <narcisbuzatu2003@gmail.com>
2021-01-18 16:37:05 +0100

    Creazione classi Scacchiera, Colore, Pezzo, Cella

---
+++ LibreriaClassi/Scacchiera.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LibreriaClassi
+{
+    class Scacchiera
+    {
+        public Cella Cella { get; private set; }
+        public List<Cella> Celle { get; private set; }
+        public Scacchiera()
+        {
+
+        }
+    }
+}
